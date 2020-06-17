84501283595a0def8af22ba5cf88d9a88f2f57c0
luis.m.barruco <luis.m.barruco@hotmail.com>
2020-06-17 09:02:37 -0300

    desafio conceitos react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,95 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-
+import api from './services/api'
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
+  	const [techs, setTechs] = useState([]);
+  	const [tech, setTech] = useState('');
+  	const [title, setTitle] = useState('');
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	useEffect(() => {
+		api.get('repositories').then(r => {
+			setRepositories(r.data);
+		});
+	}, []);
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+  	function handleTitleChange(event) {
+    	const { value } = event.target;
+    	setTitle(value);
+  	}
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+  	function handleTechChange(event) {
+    	const { value } = event.target;
+    	setTech(value);
+  	}
+
+  	async function handleAddTech() {
+    	setTechs([...techs, { index: techs.length, name: tech }])
+	}
+	  
+	async function handleAddRepository() {
+    	let rep = {
+      		title,
+      		techs: techs.map(x => x.name)
+    	};
+		
+		rep = await api.post('repositories', rep);
+    	setRepositories([...repositories, rep.data]);
+  	}
+
+  	async function handleRemoveTech(index) {
+    	techs.splice(index, 1);
+    	setTechs([...techs]);
+	}
+	  
+  	async function handleRemoveRepository(id) {
+		await api.delete(`repositories/${id}`);
+		const index = repositories.findIndex(x => x.id === id);
+    	repositories.splice(index, 1);
+    	setRepositories([...repositories]);
+  	}
+
+  	return (
+		<div>
+			<h1>Repositórios</h1>
+			<br />
+			<div className="form">
+				<div className="form-item">
+					<label>Título</label>
+					<input type="text" onChange={handleTitleChange} />
+				</div>
+
+				<div className="form-item">
+					<label>Tecnologias</label>
+					<input type="text" onChange={handleTechChange} /><button onClick={handleAddTech}>+</button>
+				</div>
+
+				<ul className="chips">
+					{techs.map(x => (
+						<li key={x.index}>
+							<div className="title">{x.name}</div>
+							<button className="remove-chip" onClick={() => handleRemoveTech(x.index)} type="button">x</button>
+						</li>
+					))}
+				</ul>
+
+				<button onClick={handleAddRepository}>Adicionar</button>
+			</div>
+
+			<div>
+				<br />
+				<ul data-testid="repository-list">
+					{repositories.map(x => (
+						<li key={x.id}>
+							{x.title}
+							<button onClick={() => handleRemoveRepository(x.id)}>Remover</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+  	);
 }
 
 export default App;
